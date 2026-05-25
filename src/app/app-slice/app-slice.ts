17f54849660031459899ad53c5b44c5b2c484020
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-25 22:11:26 +0300

    feat:create app-slice

---
+++ src/app/app-slice/app-slice.ts
@@ -0,0 +1,23 @@
+import {createSlice} from '@reduxjs/toolkit';
+
+
+
+export const appSlice = createSlice({
+  name: "app",
+  initialState: {
+    themeMode:"light" as ThemeMode
+  },
+  selectors:{
+    selectedThemeMode:(state)=>state.themeMode
+  },
+  reducers:(create)=>({
+    changeThemeModeAC:create.reducer<{ themeMode: ThemeMode }>((state,action)=>{
+      state.themeMode=action.payload.themeMode
+    })
+  })
+})
+export const{selectedThemeMode} = appSlice.selectors
+export const {changeThemeModeAC}=appSlice.actions
+export const appReducer=appSlice.reducer
+
+export type ThemeMode = 'light'|'dark'
