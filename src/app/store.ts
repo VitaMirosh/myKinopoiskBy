5bf42953ed69b2e565bcc5f302a32efd835d72da
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-25 22:09:35 +0300

    feat:create store and types(useDispatch and useSelectors)

---
+++ src/app/store.ts
@@ -0,0 +1,12 @@
+import {configureStore} from '@reduxjs/toolkit';
+import {appReducer, appSlice} from '@/app/app-slice/app-slice.ts';
+
+
+export const store = configureStore({
+  reducer: {
+    [appSlice.name]: appReducer
+  },
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
