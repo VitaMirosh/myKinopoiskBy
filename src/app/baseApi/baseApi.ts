227c5be1dfb4990e60b61ac681a09bd50bc7d761
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-05-30 15:45:29 +0300

    feat:add new endpoint for search

--- src/app/baseApi/baseApi.ts
+++ src/app/baseApi/baseApi.ts
@@ -1,20 +1,19 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 
 export const baseApi = createApi({
-  reducerPath: 'api',
-  tagTypes: ['PopularMovies'],
+  reducerPath: "api",
+  tagTypes: ["PopularMovies"],
   baseQuery: fetchBaseQuery({
     baseUrl: import.meta.env.VITE_BASE_URL,
     prepareHeaders: (headers) => {
-      // Используем только длинный токен доступа TMDB v4
-      const tmdbToken = import.meta.env.VITE_API_KEY;
+      const tmdbToken = import.meta.env.VITE_API_KEY
 
       if (tmdbToken) {
-        headers.set('Authorization', `Bearer ${tmdbToken}`);
+        headers.set("Authorization", `Bearer ${tmdbToken}`)
       }
 
-      return headers;
+      return headers
     },
   }),
   endpoints: () => ({}),
-});
+})
--- src/entities/api/cardsApi.ts
+++ src/entities/api/cardsApi.ts
@@ -1,25 +1,34 @@
-import {baseApi} from '@/app/baseApi/baseApi.ts';
-import type {BaseResponse} from '@/entities/model/types/baseResponse.ts';
-
+import { baseApi } from "@/app/baseApi/baseApi.ts"
+import type { BaseResponse, SearchResponse } from "@/entities/model/types/baseResponse.ts"
 
 const cardsApi = baseApi.injectEndpoints({
-  endpoints: build => ({
-      getPopularMovie: build.query<BaseResponse,void>({
-        query: () =>
-          '/movie/popular',
-
+  endpoints: (build) => ({
+    getPopularMovie: build.query<BaseResponse, void>({
+      query: () => "/movie/popular",
+    }),
+    getTopRatedMovie: build.query<BaseResponse, void>({
+      query: () => "/movie/top_rated",
+    }),
+    getUpcomingMovie: build.query<BaseResponse, void>({
+      query: () => "/movie/upcoming",
+    }),
+    getNowPlayingMovie: build.query<BaseResponse, void>({
+      query: () => "movie/now_playing",
+    }),
+    getSearchKeyword: build.query<SearchResponse, string>({
+      query: (search) => ({
+        url: "search/collection",
+        params: {
+          query: search,
+        },
       }),
-      getTopRatedMovie: build.query<BaseResponse,void>({
-        query: () => '/movie/top_rated'
-      }),
-      getUpcomingMovie: build.query<BaseResponse,void>({
-        query: () => '/movie/upcoming'
-      }),
-      getNowPlayingMovie: build.query<BaseResponse,void>({
-        query: () => 'movie/now_playing'
-      }),
-    }
-  ),
-
+    }),
+  }),
 })
-export const { useGetPopularMovieQuery, useGetTopRatedMovieQuery,useGetUpcomingMovieQuery,useGetNowPlayingMovieQuery} = cardsApi;
+export const {
+  useGetPopularMovieQuery,
+  useGetTopRatedMovieQuery,
+  useGetUpcomingMovieQuery,
+  useGetNowPlayingMovieQuery,
+  useGetSearchKeywordQuery,
+} = cardsApi
