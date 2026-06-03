ec94b030d5293a10a1c772e3d766866849b3c1c5
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-06-03 19:59:06 +0300

    fix:add disabled

--- src/shared/ui/Btn/index.tsx
+++ src/shared/ui/Btn/index.tsx
@@ -1,16 +1,15 @@
-import type {ReactNode} from 'react';
+import type { ReactNode } from "react"
 
 type Props = {
   children?: ReactNode
   className?: string
   onClick?: () => void
+  disabled?: boolean
 }
-export const Btn = ({children,onClick,className}:Props) => {
-
+export const Btn = ({ children, onClick, className, disabled }: Props) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button className={className} onClick={onClick} disabled={disabled}>
       {children}
     </button>
-  );
-};
-
+  )
+}
