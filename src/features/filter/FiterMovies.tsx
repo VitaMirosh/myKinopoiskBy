92928835a1a1c8d5d11dd29799bfdfce6cb28419
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-06-08 21:34:34 +0300

    feat:create filtered functional to filtered movies

---
+++ src/features/filter/FiterMovies.tsx
@@ -0,0 +1,93 @@
+import s from "./FilterMovies.module.css"
+import { type ChangeEvent, useState } from "react"
+import type { GenreResponse } from "@/entities/model/types/baseResponse.ts"
+
+type Props = {
+  genre?: GenreResponse
+  setGenreHandler: (id: number) => void
+  setPopularityHandler: (name: string) => void
+  resetAllFilters: () => void
+  setRatingMovies: (movies: number, range: number) => void
+}
+
+const arraySelect = [
+  { name: "Popularity down" },
+  { name: "Popularity up" },
+  { name: "Rating down" },
+  { name: "Rating up" },
+  { name: "Release date down" },
+  { name: "Release date up" },
+  { name: "Title A-Z" },
+  { name: "Title Z-A" },
+]
+
+export const FilterMovies = ({
+  genre,
+  setGenreHandler,
+  setPopularityHandler,
+  resetAllFilters,
+  setRatingMovies,
+}: Props) => {
+  const [range1, setRange1] = useState(0)
+  const [range2, setRange2] = useState(10)
+
+  const handleRange1 = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value)
+    if (value <= range2) {
+      setRange1(value)
+      setRatingMovies(value, range2)
+    }
+  }
+
+  const handleRange2 = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = Number(e.target.value)
+    if (value >= range1) {
+      setRange2(value)
+      setRatingMovies(range1, value)
+    }
+  }
+  const genreHandler = (id: number) => {
+    setGenreHandler(id)
+  }
+  const handlePopularityHandler = (event: ChangeEvent<HTMLSelectElement, HTMLSelectElement>) => {
+    setPopularityHandler(event.target.value)
+  }
+  const resetHandler = () => {
+    resetAllFilters()
+  }
+  return (
+    <div className={s.container}>
+      <h2> Filter / Sort</h2>
+      <div className={s.selectWrapper}>
+        <p>Sort by</p>
+        <select className={s.select} onChange={handlePopularityHandler}>
+          {arraySelect.map((item, index) => (
+            <option key={index} value={item.name}>
+              {item.name}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className={s.sliderWrapper}>
+        <p>Rating</p>
+        <p>
+          {range1.toFixed(1)} - {range2.toFixed(1)}
+        </p>
+      </div>
+      <div className={s.wrapper}>
+        <input type="range" min={0} max={10} step={0.1} value={range1} onChange={handleRange1} className={s.range} />
+        <input type="range" min={0} max={10} step={0.1} value={range2} onChange={handleRange2} className={s.range} />
+      </div>
+      <div className={s.btnWrapper}>
+        {genre?.genres?.map((g) => (
+          <button key={g.id} onClick={() => genreHandler(g.id)} className={s.btn}>
+            {g.name}
+          </button>
+        ))}
+      </div>
+      <button className={s.btnReset} onClick={resetHandler}>
+        Reset filters
+      </button>
+    </div>
+  )
+}
--- src/widgets/FilteredMovies/FilteredMovies.tsx
+++ src/widgets/FilteredMovies/FilteredMovies.tsx
@@ -1,14 +1,76 @@
-import s from "./FilteredMovies.module.css"
 import { FilterMovies } from "@/features/filter/FiterMovies.tsx"
-import { useGetPopularMovieQuery } from "@/entities/api/cardsApi.ts"
 import { Cards } from "@/shared/ui/cards/Cards.tsx"
+import { useGetGenresQuery, useGetPopularMovieQuery } from "@/entities/api/cardsApi.ts"
+import s from "./FilteredMovies.module.css"
+import { useState } from "react"
+import type { MoviesResponse } from "@/entities/model/types/baseResponse.ts"
 
 export const FilteredMovies = () => {
   const { data } = useGetPopularMovieQuery()
+  const { data: genre } = useGetGenresQuery()
+
+  const [movies, setMovies] = useState<MoviesResponse[] | undefined>()
+
+  const setGenreHandler = (id: number | null) => {
+    if (id) {
+      const genreFiltered = data?.results.filter((genre) => genre.genre_ids.includes(id))
+      setMovies(genreFiltered)
+    }
+  }
+  const setPopularityHandler = (name: string | null) => {
+    let sorted
+    if (name === "Popularity down") {
+      sorted = [...(data?.results ?? [])].sort((a, b) => (b.popularity ?? 0) - (a.popularity ?? 0))
+    }
+    if (name === "Popularity up") {
+      sorted = [...(data?.results ?? [])].sort((a, b) => (a.popularity ?? 0) - (b.popularity ?? 0))
+    }
+    if (name === "Rating down") {
+      sorted = [...(data?.results ?? [])].sort((a, b) => (b.vote_average ?? 0) - (a.vote_average ?? 0))
+    }
+    if (name === "Rating up") {
+      sorted = [...(data?.results ?? [])].sort((a, b) => (a.vote_average ?? 0) - (b.vote_average ?? 0))
+    }
+    if (name === "Release date down") {
+      sorted = [...(data?.results ?? [])].sort(
+        (a, b) => new Date(b.release_date).getTime() - new Date(a.release_date).getTime(),
+      )
+    }
+    if (name === "Release date up") {
+      sorted = [...(data?.results ?? [])].sort(
+        (a, b) => new Date(a.release_date).getTime() - new Date(b.release_date).getTime(),
+      )
+    }
+    if (name === "Title A-Z") {
+      sorted = [...(data?.results ?? [])].sort((a, b) => a.title.localeCompare(b.title))
+    }
+    if (name === "Title Z-A") {
+      sorted = [...(data?.results ?? [])].sort((a, b) => b.title.localeCompare(a.title))
+    }
+    setMovies(sorted)
+  }
+  const setRatingMovies = (min: number, max: number) => {
+    const ratedMovies = data?.results.filter(
+      (movie) => movie.vote_average && movie.vote_average >= min && movie.vote_average <= max,
+    )
+
+    setMovies(ratedMovies)
+  }
+  const resetAllFilters = () => {
+    const allMovies = data?.results.map((movie) => movie)
+    setMovies(allMovies)
+  }
+
   return (
     <div className={s.container}>
-      <FilterMovies data={data} />
-      <Cards data={data} />
+      <FilterMovies
+        setGenreHandler={setGenreHandler}
+        genre={genre}
+        setPopularityHandler={setPopularityHandler}
+        resetAllFilters={resetAllFilters}
+        setRatingMovies={setRatingMovies}
+      />
+      {!movies ? <Cards data={data} /> : <Cards filtered={movies} />}
     </div>
   )
 }
