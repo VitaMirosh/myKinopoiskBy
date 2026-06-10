8872e9dce405d72a662ff13907da1daf3a029190
VitaMirosh <miroshnichenko.vitaliya1992@gmail.com>
2026-06-10 16:09:46 +0300

    refactor:Cards,add dispatch,function to find id,add style

--- src/shared/ui/cards/Cards.tsx
+++ src/shared/ui/cards/Cards.tsx
@@ -1,29 +1,44 @@
 import s from "./Cards.module.css"
-
-import type { BaseResponse } from "@/entities/model/types/baseResponse.ts"
+import type { BaseResponse, MoviesResponse } from "@/entities/model/types/baseResponse.ts"
 import { getImageUrl } from "@/app/baseApi/baseImageApi.ts"
+import { Btn } from "@/shared/ui/Btn"
+import like from "./../../../assets/img/like.svg"
+import { useAppDispatch } from "@/app/hooks/useDispatchType.ts"
+import { toggleFavoriteMovieAC } from "@/app/app-slice/app-slice.ts"
 
 type Props = {
-  data: BaseResponse | undefined
+  data?: BaseResponse<MoviesResponse>
   className?: string
+  filtered?: MoviesResponse[]
 }
 
-export const Cards = ({ data, className }: Props) => {
+export const Cards = ({ data, className, filtered }: Props) => {
+  const dispatch = useAppDispatch()
+  const filteredResponse: MoviesResponse[] = filtered && filtered.length > 0 ? filtered : (data?.results ?? [])
+  const getFavoriteMovies = (id: number) => {
+    const movie = filteredResponse.find((m) => m.id === id)
+    if (!movie) return
+    dispatch(toggleFavoriteMovieAC({ movie }))
+  }
+
   return (
     <div className={className ? className : s.container}>
-      {data?.results.map((cart) => {
+      {filteredResponse.map((cart) => {
         return (
           <ul key={cart.id}>
             <li className={s.cart}>
-              <div className={s.vote}>{cart.vote_average?.toFixed(1)}</div>
+              <Btn className={s.btn} onClick={() => getFavoriteMovies && cart.id && getFavoriteMovies?.(cart.id)}>
+                <img src={like} />
+              </Btn>
+              {cart.vote_average && (
+                <div className={cart.vote_average > 7.5 ? s.vote : cart.vote_average > 5.5 ? s.medVote : s.smallVote}>
+                  {cart.vote_average.toFixed(1)}
+                </div>
+              )}
               {cart.poster_path && (
-                <img
-                  src={getImageUrl(cart.poster_path, "w500")}
-                  alt={cart.title}
-                  width={className ? className : "190px"}
-                  height={280}
-                  style={{ borderRadius: "15px" }}
-                />
+                <div className={s.imgWrap}>
+                  <img src={getImageUrl(cart.poster_path, "w500")} alt={cart.title} className={s.img} />
+                </div>
               )}
               {cart.title}
             </li>
